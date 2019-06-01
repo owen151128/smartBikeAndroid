bfd3c4da75522647a04f73e19fc91712010a2cda
Owen <owen151128@gmail.com>
2019-06-02 01:58:04 +0900

    Main Activity 및 layout 수정

--- app/src/main/java/kr/owens/smartBike/ui/MainActivity.java
+++ app/src/main/java/kr/owens/smartBike/ui/MainActivity.java
@@ -1,10 +1,9 @@
-package kr.owens.smartBike;
+package kr.owens.smartBike.ui;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import kr.owens.smartBike.ui.SplashActivity;
+import kr.owens.smartBike.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, SplashActivity.class));
     }
 }
