c210997966da0ecd4dda881a9835492835e5f872
owen151128 <owen151128@gmail.com>
2019-06-01 16:46:28 +0900

    사용하지 않는 클래스 제거

--- app/src/main/java/kr/owens/smartBike/MainActivity.java
+++ app/src/main/java/kr/owens/smartBike/MainActivity.java
@@ -2,7 +2,6 @@ package kr.owens.smartBike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
