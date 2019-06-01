b0c38bae6b18bd9086a878af0dccc267db258179
Owen <owen151128@gmail.com>
2019-06-02 01:49:34 +0900

    Drawable 이미지 리소스 추가

--- app/src/main/java/kr/owens/smartBike/ui/MainActivity.java
+++ app/src/main/java/kr/owens/smartBike/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.owens.smartBike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.owens.smartBike.ui.SplashActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, SplashActivity.class));
     }
 }
