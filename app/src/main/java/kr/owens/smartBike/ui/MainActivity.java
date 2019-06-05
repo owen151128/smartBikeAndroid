8a6748c71c980771504062301acceff0fa132510
owen151128 <owen151128@gmail.com>
2019-06-06 02:45:52 +0900

    MainActivity 수정 스피드 미터 크기 강제 고정

--- app/src/main/java/kr/owens/smartBike/ui/MainActivity.java
+++ app/src/main/java/kr/owens/smartBike/ui/MainActivity.java
@@ -3,13 +3,21 @@ package kr.owens.smartBike.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.LinearLayout;
+
+import com.cardiomood.android.controls.gauge.SpeedometerGauge;
+
 import kr.owens.smartBike.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPEED_METER_SIZE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SpeedometerGauge s = findViewById(R.id.speed_meter);
+        s.setLayoutParams(new LinearLayout.LayoutParams(SPEED_METER_SIZE, SPEED_METER_SIZE));
     }
 }
