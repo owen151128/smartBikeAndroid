5ef4d6de2b55a8f4de2cb5773235261400bdbf71
Owen <owen151128@gmail.com>
2019-06-09 17:41:34 +0900

    Splash 화면 요구사항에 맞게 수정 및 데이터 바인딩 추가

--- app/src/main/java/kr/owens/smartBike/ui/SplashActivity.java
+++ app/src/main/java/kr/owens/smartBike/ui/SplashActivity.java
@@ -1,17 +1,21 @@
 package kr.owens.smartBike.ui;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import kr.owens.smartBike.R;
+import kr.owens.smartBike.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding splashBinding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        splashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
 
         Thread thread = new Thread(() -> {
             try {
@@ -20,10 +24,19 @@ public class SplashActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+            findViewById(R.id.load_progress).setVisibility(View.INVISIBLE);
+            findViewById(R.id.load_text).setVisibility(View.INVISIBLE);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
         });
 
         thread.start();
     }
+
 }
